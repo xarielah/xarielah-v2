81e3594bfae49cd02167592752cbc633d6338f71
Ariel <arielah1997@gmail.com>
2023-09-01 22:26:34 +0300

    unused var

--- src/components/side-menu/side-menu-container.tsx
+++ src/components/side-menu/side-menu-container.tsx
@@ -1,5 +1,4 @@
 import { Link, useLocation } from "react-router-dom";
-import { useState } from "react";
 import sideMenuLinks from "../../lib/json/sidemenu.json";
 
 const SideMenuContainer = () => {
